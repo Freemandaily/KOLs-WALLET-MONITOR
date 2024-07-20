2bda95b7c11f4f5af6a6467761caf12f8e8b8c30
Freeman <107298762+Freemandaily@users.noreply.github.com>
2024-07-20 01:22:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,59 @@ import time,sys,os
 import requests,json
 from matic_class import maticWork
 
-processor = maticWork()
+def setUpWallet():
+    try:
+        with open('KOLWallets.txt','r') as file:
+            if file:
+                 pass
+    except:
+         print('Paste the address you want to monitor, type x to stop')
+         time.sleep(2)
+         while True:
+              address = input('Paste Address\n')
+              if address == 'x':
+                   break
+              else:
+                   try:
+                        address = Web3.to_checksum_address(address)
+                        with open('KOLWallets.txt','a') as file:
+                            file.write(address+'\n')
+                            continue
+                   except:
+                        print('Address Not Valid,Type x to exit or paste a valid address')
+                        continue
+    
+    print('SETUP IS COMPLETED')
+
+
+def setupTelegram():
+        try:
+            with open('KEY-TOKEN-ID','r') as file:
+                loadData = json.load(file)
+                botToken = loadData['BOT-TOKEN']
+                chatId = loadData['CHAT-ID']
+                key = loadData['ETHER-KEY']
+            return botToken,chatId,key
+        except:
+            explorer_key = input('Please paste your Etherscan Explorer Api-key\n')
+            time.sleep(2)
+            print('Please set Your one time Telegram bot token and chatId\n')
+            time.sleep(3)
+            token = input('What is your Telegram Bot-Token\n')
+            time.sleep(2)
+            chatId = input('What is your ChatId\n')
+            time.sleep(2)
+
+            data = {'BOT-TOKEN':token,'CHAT-ID':chatId,'ETHER-KEY': explorer_key}
+            with open('KEY-TOKEN-ID','w') as file:
+                loadData = json.dump(data,file,indent=10) 
+            return token,chatId,explorer_key
+             
+        
+token,chatId,key = setupTelegram()
+setUpWallet()
+
+processor = maticWork(token,chatId,key)
 processor.hashFetcher()
 tracking = True
 
@@ -18,4 +70,10 @@ while tracking:
         processor.off_chain()
         processor.offChain = 0
     print(processor.offChain)
-    time.sleep(10)
+    # Checking the chain every 15 minutes
+    time.sleep(300)
+
+
+              
+          
+
